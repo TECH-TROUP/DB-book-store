7af32147b99608221ae338be9c4090138e157a7d
RLUDHIYA <ludhiyargiji1@gmail.com>
2024-08-10 19:30:27 +0800

    Update create_db_script.sql

--- create_db_script.sql
+++ create_db_script.sql
@@ -24,13 +24,11 @@ CREATE TABLE IF NOT EXISTS `book_store_db`.`statuses` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `label` VARCHAR(255) NOT NULL,
   `description` TEXT NULL DEFAULT NULL,
-  `color` VARCHAR(45) NULL DEFAULT NULL,
-  `bg_color` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   UNIQUE INDEX `label_UNIQUE` (`label` ASC) VISIBLE)
 ENGINE = InnoDB
-AUTO_INCREMENT = 11
+AUTO_INCREMENT = 124
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -42,12 +40,11 @@ CREATE TABLE IF NOT EXISTS `book_store_db`.`categories` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `category_name` VARCHAR(100) NOT NULL,
   `description` TEXT NULL DEFAULT NULL,
-  `count` INT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   UNIQUE INDEX `category_name_UNIQUE` (`category_name` ASC) VISIBLE)
 ENGINE = InnoDB
-AUTO_INCREMENT = 7
+AUTO_INCREMENT = 15
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -62,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `book_store_db`.`roles` (
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   UNIQUE INDEX `role_name_UNIQUE` (`role_name` ASC) VISIBLE)
 ENGINE = InnoDB
-AUTO_INCREMENT = 7
+AUTO_INCREMENT = 10
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -86,7 +83,7 @@ CREATE TABLE IF NOT EXISTS `book_store_db`.`users` (
     FOREIGN KEY (`role_id`)
     REFERENCES `book_store_db`.`roles` (`id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 15
+AUTO_INCREMENT = 24
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -105,6 +102,11 @@ CREATE TABLE IF NOT EXISTS `book_store_db`.`books` (
   `image_url` VARCHAR(255) NULL DEFAULT NULL,
   `vendor_id` INT NOT NULL,
   `status_id` INT NOT NULL,
+  `view_count` INT NULL DEFAULT '0',
+  `purchase_count` INT NULL DEFAULT '0',
+  `borrow_count` INT NULL DEFAULT '0',
+  `rating_average` FLOAT NULL DEFAULT '0',
+  `review_count` INT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   INDEX `category_id_idx` (`category_id` ASC) VISIBLE,
@@ -120,30 +122,55 @@ CREATE TABLE IF NOT EXISTS `book_store_db`.`books` (
     FOREIGN KEY (`vendor_id`)
     REFERENCES `book_store_db`.`users` (`id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 28
+AUTO_INCREMENT = 44
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
--- Table `book_store_db`.`book_status`
+-- Table `book_store_db`.`book_copies`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `book_store_db`.`book_status` (
+CREATE TABLE IF NOT EXISTS `book_store_db`.`book_copies` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `book_id` INT NOT NULL,
   `status_id` INT NOT NULL,
-  `created_at` DATETIME NULL DEFAULT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  INDEX `book_id_idx` (`book_id` ASC) VISIBLE,
-  INDEX `status_id_idx` (`status_id` ASC) VISIBLE,
-  CONSTRAINT `individual_book_id`
+  INDEX `book_copy_book_id_idx` (`book_id` ASC) VISIBLE,
+  INDEX `book_copy_status_id_idx` (`status_id` ASC) VISIBLE,
+  CONSTRAINT `book_copy_book_id`
     FOREIGN KEY (`book_id`)
     REFERENCES `book_store_db`.`books` (`id`),
-  CONSTRAINT `status_id`
+  CONSTRAINT `book_copy_status_id`
     FOREIGN KEY (`status_id`)
     REFERENCES `book_store_db`.`statuses` (`id`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 234
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `book_store_db`.`cart`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `book_store_db`.`cart` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `book_id` INT NOT NULL,
+  `quantity` INT NOT NULL,
+  `type` ENUM('sale', 'rental') NOT NULL,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `cart_user_id_idx` (`user_id` ASC) VISIBLE,
+  INDEX `cart_book_id_idx` (`book_id` ASC) VISIBLE,
+  CONSTRAINT `cart_book_id`
+    FOREIGN KEY (`book_id`)
+    REFERENCES `book_store_db`.`books` (`id`),
+  CONSTRAINT `cart_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `book_store_db`.`users` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 8
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -237,6 +264,54 @@ CREATE TABLE IF NOT EXISTS `book_store_db`.`rentals` (
     FOREIGN KEY (`user_id`)
     REFERENCES `book_store_db`.`users` (`id`))
 ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `book_store_db`.`reviews`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `book_store_db`.`reviews` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `book_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `rating` INT NOT NULL,
+  `comment` TEXT NULL DEFAULT NULL,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `review_book_id_idx` (`book_id` ASC) VISIBLE,
+  INDEX `review_user_id_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `review_book_id`
+    FOREIGN KEY (`book_id`)
+    REFERENCES `book_store_db`.`books` (`id`),
+  CONSTRAINT `review_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `book_store_db`.`users` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `book_store_db`.`wishlist`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `book_store_db`.`wishlist` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `book_id` INT NOT NULL,
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `wishlist_user_id_idx` (`user_id` ASC) VISIBLE,
+  INDEX `wishlist_book_id_idx` (`book_id` ASC) VISIBLE,
+  CONSTRAINT `wishlist_book_id`
+    FOREIGN KEY (`book_id`)
+    REFERENCES `book_store_db`.`books` (`id`),
+  CONSTRAINT `wishlist_user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `book_store_db`.`users` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 13
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
